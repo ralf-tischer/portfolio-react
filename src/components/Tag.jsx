07d756cb9240fdbc2cbd3e3f30ef1c32f91e8555
RalfTischer <ralf.tischer.1969@gmail.com>
2024-05-27 09:21:28 +0200

    Fix issue #4 by using title tag

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,3 +1,4 @@
+import { logDOM } from '@testing-library/react';
 import React, { useState, useRef, useEffect } from 'react';
 const { myTags } = require('../model/data.js');
 
@@ -5,10 +6,6 @@ const Tag = ( {    key,
                    tagName,
                    handleTagDoubleClick} 
                 ) => {
-
-      const [showTooltip, setShowTooltip] = useState(false);
-      const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
-      const tagRef = useRef(null);
 
       const getTagProperties = (tag_short) => {
         const tagData = myTags.find(tag => tag.tag_short === tag_short);
@@ -36,82 +33,23 @@ const Tag = ( {    key,
       color: `${tagProps.color_text}`,
     };
 
-    useEffect(() => {
-      const handleScroll = () => {
-        if (tagRef.current) {
-          updateTooltipPosition();
-        }
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-  
     const onTagDoubleClick = (tagName) => {
       handleTagDoubleClick(tagName);
     };
 
-
-
-    const handleMouseEnter = (e) => {
-      setShowTooltip(true);
-      updateTooltipPosition(e);
-    };
-  
-    const handleMouseLeave = () => {
-      setShowTooltip(false);
-    };
-  
-    const updateTooltipPosition = (e) => {
-      if (tagRef.current) {
-        const { left, top, width, height } = tagRef.current.getBoundingClientRect();
-        const tooltipWidth = 200;
-        const tooltipHeight = 50;
-  
-        let x = e.clientX + 10;
-        let y = e.clientY + 10 - window.scrollY;
-  
-        if (x + tooltipWidth > window.innerWidth) {
-          x = e.clientX - tooltipWidth - 10;
-        }
-        if (y + tooltipHeight > window.innerHeight) {
-          y = e.clientY - tooltipHeight - 10 - window.scrollY;
-        }
-  
-        setTooltipPosition({ x, y });
-      }
-    };
-    
     return (
         <>
             <div 
               className='project-button tag-tag'
               style={tagStyle} 
-              onMouseEnter={handleMouseEnter} 
-              onMouseLeave={handleMouseLeave}
-              onMouseMove={updateTooltipPosition}
-              onDoubleClick={() => onTagDoubleClick(tagName)}>
+              onDoubleClick={() => onTagDoubleClick(tagName)}
+              title={tagProps.tag_long +  "\nDouble-click to set filter."}
+              >
               {tagName}
+              
             </div>
-           {showTooltip && (
-              <div
-                className="tooltip"
-                style={{
-                  position: 'absolute',
-                  backgroundColor: '#333',
-                  color: '#fff',
-                  padding: '8px',
-                  borderRadius: '4px',
-                  fontSize: '14px',
-                  zIndex: '100',
-                  left: `${tooltipPosition.x}px`,
-                  top: `${tooltipPosition.y}px`,
-                }}>
-                {tagProps.tag_long}
-              </div>
-            )}
+
+
         </>
     );
 }
