a3349251573f6ca2caa78af3d9dd4efca6abf2a7
RalfTischer <ralf.tischer.1969@gmail.com>
2024-05-28 13:40:45 +0200

    Add Home button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import SwitchColorMode from './SwitchColorMode';
 
 const Navbar = () => {
+    const navigate = useNavigate();
+
+    const handleNavigateHome = () => {
+        navigate(`/`);
+    };
+    
     return (
         <div className='navbar'>
-            <div className='project-button project-button-details' title='Click for contact details.'>Contact</div>
+            <div 
+                className='project-button project-button-details' 
+                onClick={handleNavigateHome}
+                >
+                    Home
+            </div>
+            <div 
+                className='project-button project-button-details' 
+                title='Click for contact details.'>
+                    Contact
+            </div>
             <div className='navbar-right'>
                 <SwitchColorMode />
             </div>
