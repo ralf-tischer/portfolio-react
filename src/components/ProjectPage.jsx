66165468d0d94433dc0cf3af4154c7535688c2fa
RalfTischer <ralf.tischer.1969@gmail.com>
2024-05-28 15:58:07 +0200

    Update deploy

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -1,17 +1,29 @@
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import ReactHtmlParser from 'react-html-parser';
 
 const ProjectPage = () => {
     let { id } = useParams();
+    const [projectHtml, setProjectHtml] = useState('');
 
-    // Convert id from string to number
-    id = Number(id);
-  
-    // Get project with this id
-    // ...
-  
+    useEffect(() => {
+        const loadProjectPage = async () => {
+            try {
+                const response = await fetch(`/portfolio-react/assets/projects/Project_${id}/ProjectPage_${id}.html`);
+                const data = await response.text();
+                setProjectHtml(data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        loadProjectPage();
+    }, [id]);
+
     return (
-        <p>ProjectPage {id}</p>
-    )
-}
+        <>
+            {ReactHtmlParser(projectHtml)}
+        </>
+    );
+};
 
 export default ProjectPage;
