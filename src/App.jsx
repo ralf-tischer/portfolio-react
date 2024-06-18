32b68cecaa4c1b4e0736fbbe541f263c7cac6d91
RalfTischer <ralf.tischer.1969@gmail.com>
2024-06-18 17:57:49 +0200

    Update navigation.
    Add MainContent() for intro, projects and contact
    Add ContactRoute() to smoothly scroll to contact
    Add handleRoute() to smoothly scroll to elements
    Update render to MainContent

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // was BrowserRouter
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Projects from './components/Projects.jsx';
@@ -7,27 +8,56 @@ import Contact from './components/Contact.jsx';
 import Introduction from './components/Introduction.jsx';
 
 const baseUrl = "/portfolio-react";
-//const baseUrl = "/";
 
 const App = () => {
-  let { tags, ids } = useParams();
-
   const handleTagClick = (tag) => {
     console.log("Tagging not from here!");
+  }
+
+  const MainContent = () => {
+    const { tags, ids } = useParams();
+
+    useEffect(() => {
+      if (tags || ids) {
+        const element = document.getElementById('projects');
+        if (element) element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, [tags, ids]);
+
+    return (
+      <>
+        <div id="introduction"><Introduction /></div>
+        <div id="projects"><Projects tags={tags} ids={ids} handleRoute={handleRoute} /></div>
+        <div id="contact"><Contact /></div>
+      </>
+    );
+  }
+
+  const ContactRoute = () => {
+    useEffect(() => {
+      const element = document.getElementById('contact');
+      if (element) element.scrollIntoView({ behavior: 'smooth' });
+    }, []);
+
+    return <Contact />;
+  }
+
+  const handleRoute = (route) => {
+    console.log("Route: ", route);
+    const element = document.getElementById(route);
+    if (element) element.scrollIntoView({ behavior: 'smooth' });
   }
 
   return (
     <Router basename={baseUrl}>
       <div className='container'>
-        <Navbar />
+        <Navbar handleRoute={handleRoute} />
         <Routes>
-          <Route path="/tags/:tags" element={<Projects tags={tags} />} />
-          <Route path="/ids/:ids" element={<Projects ids={ids} />} />
-          <Route path="/id/:id" element={<ProjectPage handleTagClick={handleTagClick} />}  />
-          <Route path="/contact/" element={<Contact />} />
-          <Route path="/introduction/" element={<Introduction />} />
-          <Route path="/projects/" element={<Projects />} /> 
-          <Route path="/" element={<Introduction />} />    
+          <Route path="/" element={<MainContent />} />
+          <Route path="/tags/:tags" element={<MainContent />} />
+          <Route path="/ids/:ids" element={<MainContent />} />
+          <Route path="/id/:id" element={<ProjectPage handleTagClick={handleTagClick} />} />
+          <Route path="/contact/" element={<ContactRoute />} />
         </Routes>
       </div>
     </Router>
