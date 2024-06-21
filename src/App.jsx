2fd0b03abe95c0af03949829243a0776c75f5cb3
Ralf Tischer <108927724+ralf-tischer@users.noreply.github.com>
2024-06-21 14:30:33 +0000

    Update for improved scrolling: target: useState, MainPageWrapper, ProjectPageWrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
@@ -7,22 +7,35 @@ import ProjectPage from './components/ProjectPage.jsx';
 import Contact from './components/Contact.jsx';
 import Introduction from './components/Introduction.jsx';
 
-const baseUrl = "/portfolio-react";
+const baseUrl = window.location.pathname;
 
 const App = () => {
+  const [target, setTarget] = useState(null);
+  
+  useEffect(() => {
+    console.log("useEffect Route: ", target);
+    const element = document.getElementById(target);
+    if (element) { 
+      console.log("useEffect Element: ", element);
+      element.scrollIntoView({ behavior: 'smooth' })
+    };
+  }, [target]);
+
   const handleTagClick = (tag) => {
     console.log("Tagging not from here!");
   }
 
-  const MainContent = () => {
+  const MainPageWrapper = () => {
     const { tags, ids } = useParams();
 
+    /*
     useEffect(() => {
       if (tags || ids) {
         const element = document.getElementById('projects');
         if (element) element.scrollIntoView({ behavior: 'smooth' });
       }
     }, [tags, ids]);
+    */
 
     return (
       <>
@@ -33,35 +46,40 @@ const App = () => {
     );
   }
 
-  const ContactRoute = () => {
-    useEffect(() => {
-      const element = document.getElementById('contact');
-      if (element) element.scrollIntoView({ behavior: 'smooth' });
-    }, []);
-
-    return <Contact />;
+  const ProjectPageWrapper = () => {
+    //<Route path="/id/:id" element={<ProjectPage handleTagClick={handleTagClick} />} />
+    return (
+      <ProjectPage handleTagClick={handleTagClick} />
+    )
   }
 
+
   const handleRoute = (route) => {
-    console.log("Route: ", route);
-    const element = document.getElementById(route);
-    if (element) element.scrollIntoView({ behavior: 'smooth' });
+    setTarget(route);
   }
 
   return (
-    <Router basename={baseUrl}>
-      <div className='container'>
+    <div className='container'>
+      <Router basename={baseUrl}>
         <Navbar handleRoute={handleRoute} />
         <Routes>
+          <Route path="/id/:id" element={<ProjectPageWrapper handleTagClick={handleTagClick} />} />
+          <Route path="/tags/:tags" element={<MainPageWrapper target={'projects'}/>} />
+          <Route path="/ids/:ids" element={<MainPageWrapper target={'projects'}/>} />
+          <Route path="/contact" element={<MainPageWrapper target={'contact'}/>} />
+          <Route path="/" element={<MainPageWrapper target={'introduction'}/>} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App;
+
+/*
           <Route path="/" element={<MainContent />} />
           <Route path="/tags/:tags" element={<MainContent />} />
           <Route path="/ids/:ids" element={<MainContent />} />
           <Route path="/id/:id" element={<ProjectPage handleTagClick={handleTagClick} />} />
           <Route path="/contact/" element={<ContactRoute />} />
-        </Routes>
-      </div>
-    </Router>
-  )
-}
-
-export default App;
+*/
