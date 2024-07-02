899069a4a26feac33cbe9281326d52c146f9eb85
Ralf Tischer <108927724+ralf-tischer@users.noreply.github.com>
2024-07-02 12:09:04 +0000

    Fix isuue #36: tags as useState, MainWrapper as separate file, ids and tags as hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { HashRouter as Router, Route, Routes, useParams } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
-import Projects from './components/Projects.jsx';
+import MainPageWrapper from './components/MainPageWrapper.jsx';
 import ProjectPage from './components/ProjectPage.jsx';
-import Contact from './components/Contact.jsx';
-import Introduction from './components/Introduction.jsx';
 
 //const baseUrl = process.env.PUBLIC_URL || (window.location.pathname.includes('portfolio-react') ? '/portfolio-react' : '/');
 const baseUrl = "/";
@@ -13,9 +11,14 @@ console.log('window.location.pathname:', window.location.pathname);
 console.log('process.env.PUBLIC_URL:', process.env.PUBLIC_URL);
 console.log('baseUrl:', baseUrl);
 
-
 const App = () => {
+  
+  let { tags: tagsInitial, ids } = useParams();
+  tagsInitial = tagsInitial ? tagsInitial.split(',') : [];
+  ids = ids ? ids.split(',').map(Number) : [];
+
   const [target, setTarget] = useState(null);
+  const [tags, setTags] = useState(tagsInitial);
   
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -29,32 +32,8 @@ const App = () => {
   }, [target]);
 
   const handleTagClick = (tag) => {
-    console.log("Tag clicked:", tag); // Simulated use of 'tag'
+    setTags([tag]);
     setTarget('projects');
-  }
-
-  const MainPageWrapper = ({ handleTagClick }) => {
-    let { tags, ids } = useParams();
-    tags = tags ? tags.split(',') : [];
-    ids = ids ? ids.split(',').map(Number) : [];
-
-    console.log('tags:', tags);
-    console.log('ids:', ids);
-    
-    useEffect(() => {
-      if (tags || ids) {
-        const element = document.getElementById('projects');
-        if (element) element.scrollIntoView({ behavior: 'smooth' });
-      }
-    }, [tags, ids]);
-
-    return (
-      <>
-        <div id="introduction"><Introduction handleTagClick={handleTagClick} /></div>
-        <div id="projects"><Projects tags={tags} ids={ids} handleRoute={handleRoute} handleTagClick={handleTagClick} /></div>
-        <div id="contact"><Contact /></div>
-      </>
-    );
   }
 
   const ProjectPageWrapper = ({ handleTagClick }) => {
@@ -65,6 +44,7 @@ const App = () => {
   }
 
   const handleRoute = (route) => {
+    if (route === 'projects') setTags([]);
     setTarget(route);
   }
 
@@ -74,10 +54,10 @@ const App = () => {
         <Navbar handleRoute={handleRoute} />
         <Routes>
           <Route path="/id/:id" element={<ProjectPageWrapper handleTagClick={handleTagClick} />} />
-          <Route path="/tags/:tags" element={<MainPageWrapper target={'projects'} handleTagClick={handleTagClick} />} />
-          <Route path="/ids/:ids" element={<MainPageWrapper target={'projects'} handleTagClick={handleTagClick} />} />
-          <Route path="/contact" element={<MainPageWrapper target={'contact'} handleTagClick={handleTagClick} />} />
-          <Route path="/" element={<MainPageWrapper target={'introduction'} handleTagClick={handleTagClick} />} />
+          <Route path="/tags/:tags" element={<MainPageWrapper ids={ids} tags={tags} handleTagClick={handleTagClick} />} />
+          <Route path="/ids/:ids" element={<MainPageWrapper ids={ids} tags={tags} handleTagClick={handleTagClick} />} />
+          <Route path="/contact" element={<MainPageWrapper ids={ids} tags={tags} handleTagClick={handleTagClick} />} />
+          <Route path="/" element={<MainPageWrapper ids={ids} tags={tags} handleTagClick={handleTagClick} />} />
         </Routes>
       </Router>
     </div>
---
+++ src/components/MainPageWrapper.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import Introduction from './Introduction.jsx';
+import Projects from './Projects.jsx';
+import Contact from './Contact.jsx';
+
+const MainPageWrapper = ({ ids, tags, handleTagClick, handleRoute }) => {
+    
+    useEffect(() => {
+      if (tags.length > 0 || ids.length > 0) {
+        const element = document.getElementById('projects');
+        if (element) element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, [tags, ids]);
+
+    return (
+      <>
+        <div id="introduction"><Introduction handleTagClick={handleTagClick} /></div>
+        <div id="projects"><Projects tags={tags} ids={ids} handleRoute={handleRoute} handleTagClick={handleTagClick} /></div>
+        <div id="contact"><Contact /></div>
+      </>
+    );
+}
+
+export default MainPageWrapper;
