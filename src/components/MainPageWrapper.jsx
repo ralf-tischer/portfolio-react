9b7cb547115491a55ae137657708bc9a65d8ef59
Ralf Tischer <108927724+ralf-tischer@users.noreply.github.com>
2024-07-03 11:52:55 +0000

    Fix issue #40: Remove handleRoute hook

--- src/components/MainPageWrapper.jsx
+++ src/components/MainPageWrapper.jsx
@@ -3,7 +3,7 @@ import Introduction from './Introduction.jsx';
 import Projects from './Projects.jsx';
 import Contact from './Contact.jsx';
 
-const MainPageWrapper = ({ ids, tags, handleTagClick, handleRoute }) => {
+const MainPageWrapper = ({ ids, tags, handleTagClick }) => {
     
     useEffect(() => {
       if (tags.length > 0 || ids.length > 0) {
@@ -15,7 +15,7 @@ const MainPageWrapper = ({ ids, tags, handleTagClick, handleRoute }) => {
     return (
       <>
         <div id="introduction"><Introduction handleTagClick={handleTagClick} /></div>
-        <div id="projects"><Projects tags={tags} ids={ids} handleRoute={handleRoute} handleTagClick={handleTagClick} /></div>
+        <div id="projects"><Projects tags={tags} ids={ids} handleTagClick={handleTagClick} /></div>
         <div id="contact"><Contact /></div>
       </>
     );
